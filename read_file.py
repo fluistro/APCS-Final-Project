dcc004f45b68c5a7654b8e41d1ffddef273bc231
gavinceee <86315937+gavinceee@users.noreply.github.com>
2023-05-11 13:34:30 -0700

    Comment

---
+++ read_file.py
@@ -0,0 +1,20 @@
+'''
+{
+    'course1': {
+        'Base Terms/Year': 0,
+        'Covered Terms/Year': 0,
+        'Max Enrollment': 30,
+        'PPC': 0,
+        'Priority': 0,
+        'Sections': 0,
+        'Prereqs': 0,
+        'Postreqs': 0,
+        'Sim With this course': 0,
+        'no sim With this course': 0,
+        'Selected by students': 0,
+    },
+    ...
+}
+
+
+'''
