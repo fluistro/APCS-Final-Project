ffb32b807cfba0a60b4c92aa1dd9d39018ae1b5d
gavinceee <gcliclig@gmail.com>
2023-05-15 14:58:17 -0700

    gavin student requess may 15 numba 1

--- read_file.py
+++ read_file.py
@@ -1,9 +1,11 @@
 import csv
+'''
+
 with open('Course Sequencing Rules.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
         print(', '.join(row))
-'''
+
 {
     'course code 1': {
         'course name': "",
@@ -32,6 +34,28 @@ course_info = {}
 
 
 # read Cleaned Student Requests
+current_student_id = 0
+skip_header_line = False
+current_line_splitted = ""
+with open('Cleaned Student Requests.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        current_line_splitted = row.split(",")
+        
+        # if current line is the start of a new student
+        if (len(current_line_splitted) == 2):
+            skip_header_line = True
+            current_student_id = current_line_splitted[1]
+
+        # if current line is not the start of a new student
+        else:
+            # if its the header line
+            if (skip_header_line == True):
+                continue
+
+            skip_header_line = False
+
+
 
 
 
